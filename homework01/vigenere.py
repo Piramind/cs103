452607fe501ee99ed0601333f0f11620434262bc
Piramind <tikhonov.lev0@gmail.com>
2019-10-11 23:27:42 +0300

    Реализована функция decrypt

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -25,3 +25,29 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
         ciphertext += chr(code)
     return ciphertext
 
+def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    plaintext = ''
+    text_len = len(ciphertext)
+    key_len = len(keyword)
+    new_key = list()
+    for i in range(text_len):
+        new_key.append(keyword[i % key_len])
+    shifts = list()
+    for char in new_key:
+        code = ord(char)
+        if 97 <= code <= 122:
+            shifts.append(code - 97)
+        elif 65 <= code <= 90:
+            shifts.append(code - 65)
+    for index, char in enumerate(ciphertext):
+        code = ord(char)
+        if 97 <= code <= 122:
+            code -= shifts[index]
+            if not 97 <= code <= 122:
+                code += 26
+        elif 65 <= code <= 90:
+            code -= shifts[index]
+            if not 65 <= code <= 90:
+                code += 26
+        plaintext += chr(code)
+    return plaintext

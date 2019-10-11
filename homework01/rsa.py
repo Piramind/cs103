1d61265c5e8d791db1c19a645ec8d7cdb32fc957
Piramind <tikhonov.lev0@gmail.com>
2019-10-11 23:49:14 +0300

    Добавлена gcd

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -6,3 +6,10 @@ def is_prime(num: int) -> bool:
         if num % i == 0:
             return False
     return True
+def gcd(a: int, b: int) -> int:
+    for i in range(a - 1, 1, -1):
+        for j in range(b - 1, i - 1, -1):
+            if i == j and a % i == 0 and b % i == 0:
+                return i
+
+

dbf05b0aa3ab3644ca4c320661cfcb8b0f81704a
Piramind <tikhonov.lev0@gmail.com>
2019-10-11 23:55:04 +0300

    Реализован алгоритм поиска НОД и generate key

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -6,6 +6,8 @@ def is_prime(num: int) -> bool:
         if num % i == 0:
             return False
     return True
+
+
 def gcd(a: int, b: int) -> int:
     for i in range(a - 1, 1, -1):
         for j in range(b - 1, i - 1, -1):
@@ -13,3 +15,37 @@ def gcd(a: int, b: int) -> int:
                 return i
 
 
+def multiplicative_inverse(e: int, phi: int) -> int:
+    div = []
+    rows = 0
+    phi_temp = phi
+    while phi_temp % e != 0:
+        div.append(phi_temp // e)
+        phi_temp, e = e, phi_temp % e
+        rows += 1
+    x = 0
+    y = 1
+    for i in range(rows - 1, -1, -1):
+        x, y = y, x - (y * div[i])
+    return y % phi
+
+
+def generate_keypair(p: int, q: int) -> ((int, int), (int, int)):
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+
+    n = p * q
+
+    phi = (p - 1) * (q - 1)
+
+    e = random.randrange(1, phi)
+
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    d = multiplicative_inverse(e, phi)
+    return (e, n), (d, n)

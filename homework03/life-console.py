9647d9a7b15979d2ed983c473a265f2227601d63
Piramind <56030270+Piramind@users.noreply.github.com>
2019-11-09 15:09:06 +0300

    Update life-console.py

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -3,7 +3,13 @@ import time
 import pygame
 
 from life import GameOfLife
-from ui import UI
+class UI(abc.ABC):
+    def __init__(self, life: GameOfLife):
+        self.life = life
+
+    @abc.abstractmethod
+    def run(self):
+        pass
 
 
 class Console(UI):
